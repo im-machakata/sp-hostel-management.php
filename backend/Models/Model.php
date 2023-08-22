bccaeb296c24bdb63ddc3edfe94936d2e6e74a14
Isaac Machakata <isaac@caasi.co.zw>
2023-08-22 23:34:53 +0200

    refactor: log database error feat: add findWhere feat: add find()

--- backend/Models/Model.php
+++ backend/Models/Model.php
@@ -1,33 +1,73 @@
  <?php
-include_once __DIR__ . '/../config.php';
-include_once __DIR__ . '/../System/Database.php';
+    include_once __DIR__ . '/../config.php';
+    include_once __DIR__ . '/../System/Database.php';
 
-class Model
-{
-    protected $db;
-    protected $table;
-    protected $primaryId = 'id';
-    private array $errors = [];
-
-    public function find($id) {}
-
-    public function save(array $data) {}
-
-    public function hasErrors(): bool
+    class Model
     {
-        return $this->errors ? true : false;
-    }
+        protected $db;
+        protected $table;
+        protected $primaryId = 'id';
+        protected array $errors = [];
 
-    /**
-     * @return bool|null
-     */
-    public function getLastError()
-    {
-        return end($this->errors);
-    }
+        public function __construct()
+        {
+            $this->db = new Database();
+            if (!$this->db->isConnected()) {
+                $this->errors[] = $this->db->getErrors()[0];
+            }
+        }
 
-    public function getErrors(): array
-    {
-        return $this->errors;
+        public function find($id)
+        {
+            $this->db->prepare(
+                sprintf('Select * FROM %s WHERE %s = ":id"', $this->table, $this->primaryId),
+                ['id' => $id]
+            );
+            $this->db->exec();
+            return $this;
+        }
+        public function getResults()
+        {
+            return $this->db->getRows();
+        }
+
+        public function save(array $data)
+        {
+        }
+        public function findWhere($conditions = [])
+        {
+            $itemIndex = 0;
+            $preparedConditions = [];
+            foreach ($conditions as $key => $value) {
+                if ($itemIndex == 0) {
+                    $preparedConditions[]  = sprintf(' WHERE `%s`=\'%s\'', $key, $value);
+                } else {
+                    $preparedConditions[] = sprintf(' AND `%s`=\'%s\'', $key, $value);
+                }
+                $itemIndex++;
+            }
+            $this->db->prepare(
+                sprintf('Select * FROM %s', $this->table, implode('', $preparedConditions))
+            );
+            $this->db->exec();
+            return $this;
+        }
+
+        public function hasErrors(): bool
+        {
+            return $this->errors ? true : false;
+        }
+
+        /**
+         * @return bool|null
+         */
+        public function getLastError()
+        {
+            return end($this->errors);
+        }
+
+        public function getErrors(): array
+        {
+            return $this->errors;
+        }
     }
-}

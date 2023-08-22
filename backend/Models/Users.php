ee275444eb902532ede807600d038b31fd85eeda
Isaac Machakata <isaac@caasi.co.zw>
2023-08-22 23:36:52 +0200

    feat: add login function

--- backend/Models/Users.php
+++ backend/Models/Users.php
@@ -1,4 +1,5 @@
 <?php
+include_once 'Model.php';
 class Users extends Model
 {
     protected $table = "users";
@@ -7,8 +8,13 @@ class Users extends Model
 
     public function login($studentNumber, $studentPassword): bool
     {
+        $this->findWhere(['username' => $studentNumber, $studentPassword]);
+        $result = $this->getResults() ? true : false;
+        $this->errors[] = !$result ? "Username or password is incorrect." : null;
+        return $result;
     }
     public function register($studentNumber, $studentPassword): bool
     {
+        return true;
     }
 }

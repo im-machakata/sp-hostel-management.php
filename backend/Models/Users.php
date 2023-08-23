989c60277e9771de54289b43cb8a3cf35bd64375
Isaac Machakata <isaac@caasi.co.zw>
2023-08-23 11:06:05 +0200

    refactor: login

--- backend/Models/Users.php
+++ backend/Models/Users.php
@@ -3,15 +3,23 @@ include_once 'Model.php';
 class Users extends Model
 {
     protected $table = "users";
-    private string $studentNumber;
-    private string $studentPassword;
 
-    public function login($studentNumber, $studentPassword): bool
+    public function login($username, $password): bool
     {
-        $this->findWhere(['username' => $studentNumber, $studentPassword]);
-        $result = $this->getResults() ? true : false;
-        $this->errors[] = !$result ? "Username or password is incorrect." : null;
-        return $result;
+        // find user by student number
+        $this->findWhere(['username' => $username]);
+        if (!$user = $this->getResults()) {
+            $this->errors[] = "No user found with that username.";
+            return false;
+        }
+
+        $user = $user[0];
+        // confirm if password is correct
+        if (!password_verify($password, $user['password'])) {
+            $this->errors[] = "You have entered an invalid password.";
+            return false;
+        }
+        return true;
     }
     public function register($studentNumber, $studentPassword): bool
     {

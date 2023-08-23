81ab17b84974c477d4b18aae34122201878fad28
Isaac Machakata <isaac@caasi.co.zw>
2023-08-23 21:46:35 +0200

    feat: add footer component

---
+++ frontend/components/footer.php
@@ -0,0 +1,5 @@
+<footer class="d-flex flex-wrap justify-content-center align-items-center py-3 my-4 border-top border-success">
+    <div class="d-flex align-items-center">
+        <span class="text-muted">Copyright of &copy;<?= SCHOOL_NAME . ', ' . date('Y') ?></span>
+    </div>
+</footer>

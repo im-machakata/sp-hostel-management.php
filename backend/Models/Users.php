5cc1d4e477a6f16fd45f7d678d78c3e640e765b3
Isaac Machakata <isaac@caasi.co.zw>
2023-08-23 11:43:40 +0200

    feat: create a new user

--- backend/Models/Users.php
+++ backend/Models/Users.php
@@ -21,8 +21,40 @@ class Users extends Model
         }
         return true;
     }
-    public function register($studentNumber, $studentPassword): bool
+    public function register($studentNumber, $studentPassword, bool $isAdmin = false): bool
     {
+        // validate if both username and password are present
+        if (!$studentNumber || !$studentPassword) {
+            $this->errors[] = "Student Number or Password is missing.";
+            return false;
+        }
+
+        // check if username / user id is not taken
+        if ($this->findWhere(['username' => $studentNumber])->getResults()) {
+            $this->errors[] = 'Username is not available, try another one.';
+            return false;
+        }
+
+        #region Register User
+        // save the user to the database
+        try {
+            $this->db->prepare(
+                sprintf('INSERT INTO %s (id,username,password,is_admin) VALUES (NULL,:username,:password,:is_admin)', $this->table),
+                array(
+                    'username' => $studentNumber,
+                    'password' => password_hash($studentPassword, PASSWORD_DEFAULT),
+                    'is_admin' => $isAdmin ? '1' : '0'
+                )
+            );
+            $this->db->exec();
+        } catch (PDOException $e) {
+
+            // if an error occured, log it
+            $this->errors[] = $e->getMessage();
+            return false;
+        }
+        #endregion
+
         return true;
     }
 }

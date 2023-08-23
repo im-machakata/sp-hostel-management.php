4777a0d2c2b657b4435c3b1fd5bab5637b557dd8
Isaac Machakata <isaac@caasi.co.zw>
2023-08-23 22:34:22 +0200

    refactor: add script tag

--- backend/Views/footer.php
+++ backend/Views/footer.php
@@ -3,3 +3,4 @@
         <span class="text-muted">Copyright of &copy;<?= SCHOOL_NAME . ', ' . date('Y') ?></span>
     </div>
 </footer>
+<script src="/assets/js/bootstrap.bundle.min.js" defer></script>

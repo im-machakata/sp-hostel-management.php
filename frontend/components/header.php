490652067ff31b174feaf17cf7a668d3dc3535ac
Isaac Machakata <isaac@caasi.co.zw>
2023-08-23 21:52:46 +0200

    feat: add header component

---
+++ frontend/components/header.php
@@ -0,0 +1,7 @@
+<header>
+    <h1 class="text-lg-center pt-4 display-4 align-items-center">
+        <span class="badge p-0 text-success">Hostelia</span>
+        <span class="badge text-dark p-0 fw-normal"><?= $page ?></span>
+    </h1>
+    <p class="lead text-lg-center">The ultimate solution for all students looking for accomadation.</p>
+</header>

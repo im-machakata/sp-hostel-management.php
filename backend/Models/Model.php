4e9e2f1c547b5e9cfe124485c3ecc62dc06295de
Isaac Machakata <isaac@caasi.co.zw>
2023-08-23 11:54:02 +0200

    feat: add get Row feat: save table data

--- backend/Models/Model.php
+++ backend/Models/Model.php
@@ -20,11 +20,15 @@
         public function find($id)
         {
             $this->db->prepare(
-                sprintf('Select * FROM %s WHERE %s = ":id"', $this->table, $this->primaryId),
+                sprintf('Select * FROM %s WHERE %s = ":id" LIMIT 0,1', $this->table, $this->primaryId),
                 ['id' => $id]
             );
             $this->db->exec();
             return $this;
+        }
+        public function getRow()
+        {
+            return $this->db->getRow();
         }
         public function getResults()
         {
@@ -33,6 +37,22 @@
 
         public function save(array $data)
         {
+            $keys_index = 0;
+            $cols = $vals = [];
+            foreach ($data as $col => $value) {
+                $keys_index++;
+                if ($col == $this->primaryId) continue;
+                $last_col = $keys_index == count($data);
+                $cols[] = sprintf("`%s%s`", $col, $last_col ? '' : ', ');
+                $vals[] = sprintf(":%s%s", $value, $last_col ? '' : ', ');
+            }
+
+            // we are updatin the table
+            if (array_key_exists($this->primaryId, $data)) {
+                $this->db->prepare(sprintf('UPDATE %s SET %s WHERE %s = :id', $this->table, implode('', $cols), $this->primaryId), $vals);
+            } else {
+                $this->db->prepare(sprintf('INSERT INTO %s (%s) VALUES(%s)', $this->table, implode('', $cols), $this->primaryId), $vals);
+            }
         }
         public function findWhere($conditions = [])
         {
@@ -46,9 +66,7 @@
                 }
                 $itemIndex++;
             }
-            $this->db->prepare(
-                sprintf('Select * FROM %s', $this->table, implode('', $preparedConditions))
-            );
+            $this->db->prepare(sprintf('SELECT * FROM %s %s', $this->table, implode('', $preparedConditions)));
             $this->db->exec();
             return $this;
         }
@@ -58,8 +76,12 @@
             return $this->errors ? true : false;
         }
 
+        public function getFirstError()
+        {
+            return $this->errors[0];
+        }
         /**
-         * @return bool|null
+         * @return string|null
          */
         public function getLastError()
         {

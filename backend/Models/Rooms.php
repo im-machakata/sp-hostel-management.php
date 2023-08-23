16954039d430696a6500dd51c2172dd355a7fe6c
Isaac Machakata <isaac@caasi.co.zw>
2023-08-23 22:40:22 +0200

    refactor: fetch queries

--- backend/Models/Rooms.php
+++ backend/Models/Rooms.php
@@ -1,22 +1,28 @@
 <?php
+include_once 'Model.php';
 class Rooms extends Model
 {
+    protected $table = 'rooms';
     public function book(): bool
     {
     }
-    public function isBooked(): bool
+    public function isBooked($id): bool
     {
+        return empty($this->findWhere(['id' => $id, 'is_booked' => '1'])->getRow());
     }
     public function free(): bool
     {
     }
-    public function isFree(): bool
+    public function isFree($id): bool
     {
+        return !empty($this->findWhere(['id' => $id, 'is_booked' => '1'])->getRow());
     }
     public function getFreeRooms(): array
     {
+        return $this->findWhere(['is_booked' => '0'])->getResults();
     }
     public function getBookedRooms(): array
     {
+        return $this->findWhere(['is_booked' => '1'])->getResults();
     }
 }

7217120afe3f4a88385276ce6b125f19a3d97eef
Isaac Machakata <isaac@caasi.co.zw>
2023-08-23 22:52:24 +0200

    style: form inputs refactor: use head component

--- frontend/create-account.php
+++ frontend/create-account.php
@@ -4,14 +4,12 @@ $controller = new RegisterController();
 
 render_component('head', ['title' => 'Register']); ?>
 
-
 <body>
     <main class="container-fluid">
         <section class="login row jumbotron justify-content-center">
             <!-- Header -->
             <div class="col-11 col-lg-12 text-lg-center">
-                <h1 class="pt-4 mt-lg-4 display-5">Sign Up > Hostelia</h1>
-                <p class="lead">The ultimate solution for all students looking for accomadation.</p>
+                <?php render_component('header', ['page' => 'Register']); ?>
             </div>
 
             <!-- Errors Layouts -->
@@ -25,13 +23,13 @@ render_component('head', ['title' => 'Register']); ?>
             <!-- Register Form -->
             <div class="col-10 col-lg-6 my-2 border border-success rounded p-3 p-lg-4 mb-4">
                 <form action="/create-account.php" method="post">
-                    <div class="mb-3">
-                        <label for="student-id" class="form-label">Student ID</label>
+                    <div class="mb-3 form-floating">
                         <input type="text" class="form-control" placeholder="Student ID" id="student-id" name="student-id" required>
+                        <label for="student-id" class="form-label">Student ID</label>
                     </div>
-                    <div class="mb-4">
-                        <label for="password" class="form-label">Password</label>
+                    <div class="mb-4 form-floating">
                         <input type="password" class="form-control" placeholder="Password" id="password" name="password" required>
+                        <label for="password" class="form-label">Password</label>
                     </div>
                     <button type="submit" class="btn btn-success btn-lg w-100">Create Account</button>
                     <p class="mt-4 mb-1 text-lg-center">Already have an account? <a href="/login.php">Login</a> to your account.</p>

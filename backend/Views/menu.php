c04bdbfd0728cdeade2d1a841058fe00079169dc
Isaac Machakata <isaac@caasi.co.zw>
2023-08-24 10:39:16 +0200

    style: round button

--- backend/Views/menu.php
+++ backend/Views/menu.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-light border-bottom border-success shadow-sm">
     <div class="container-fluid">
         <a class="navbar-brand fw-bold" href="/"><span class="text-success">Hostel</span><span class="text-muted">ia</span></a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler rounded" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">

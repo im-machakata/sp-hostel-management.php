8d37dd264d2b5222242b857e4a1f9d799555060d
Isaac Machakata <isaac@caasi.co.zw>
2023-08-24 08:36:08 +0200

    refactor: add demo image

--- frontend/index.php
+++ frontend/index.php
@@ -1,25 +1,48 @@
 <?php
-require_once "../backend/Controllers/HomeController.php";
+require "../backend/Controllers/HomeController.php";
+require '../backend/Models/Rooms.php';
+
 $controller = new HomeController();
+$rooms = new Rooms();
+$available_rooms = $rooms->getFreeRooms();
+$all_rooms = $rooms->countAllRooms()['total'];
+$percentage_free = number_format(((count($available_rooms) / $all_rooms) * 100));
+
+render_component('head', ['title' => 'Home']);
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/assets/css/bootstrap-reboot.min.css">
-    <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
-    <title>Hostel Management System</title>
-</head>
 <body>
+    <?php render_component('menu'); ?>
     <main class="container-fluid">
-        <h1 class="text-center pt-4">Hostels Management System</h1>
-        <p class="lead text-center">The ultimate solution for all students looking for accomadation.</p>
-
-        <section class="showcase jumbotron"></section>
-    </main>    
-
-<script src="/assets/js/bootstrap.bundle.min.js"></script>
+        <?php render_component('header', ['page' => 'Dashboard']); ?>
+        <section class="row justify-content-center">
+            <div class="col-lg-6 mb-3 mb-lg-4">
+                <div class="progress">
+                    <div class="progress-bar bg-warning progress-bar-striped progress-bar-animated" role="progressbar" style="width: <?= $percentage_free ?>%" aria-valuenow="<?= $percentage_free ?>" aria-valuemin="0" aria-valuemax="100" data-bs-toggle="tooltip" data-bs-placement="left" title="Free Rooms"></div>
+                    <div class="progress-bar bg-success progress-bar-striped progress-bar-animated" role="progressbar" style="width: <?= (100 - $percentage_free) ?>%" aria-valuenow="<?= (100 - $percentage_free) ?>" aria-valuemin="0" aria-valuemax="100" data-bs-toggle="tooltip" data-bs-placement="right" title="Booked Rooms"></div>
+                </div>
+            </div>
+            <div class="col-12"></div>
+            <?php foreach ($available_rooms as $room) : ?>
+                <div class="col-sm-6 col-lg-3">
+                    <div class="card border-success">
+                        <div class="ratio ratio-16x9">
+                            <img src="<?= $room['image_url'] ?? ('/assets/images/demo.jpg') ?>" class="card-img-top img-fluid" alt="<?= $room['name'] ?> Image">
+                        </div>
+                        <div class="card-body">
+                            <div class="text-dark h5 badge bg-warning mb-2">
+                                USD $<?= number_format($room['cost'], 2) ?>
+                            </div>
+                            <h2 class="card-title"><?= $room['name'] ?></h2>
+                            <p class="card-text"><?= $room['description'] ?></p>
+                            <a href="/book-room.php?id=<?= $room['id'] ?>" class="btn btn-outline-success">Book This Room</a>
+                        </div>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        </section>
+    </main>
+    <?php render_component('footer'); ?>
 </body>
+
 </html>

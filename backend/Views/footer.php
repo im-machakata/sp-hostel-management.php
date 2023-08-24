745351527827429bbd59fc96939ee7c97c4ede3c
Isaac Machakata <isaac@caasi.co.zw>
2023-08-24 07:57:34 +0200

    style: add top margin

--- backend/Views/footer.php
+++ backend/Views/footer.php
@@ -1,4 +1,4 @@
-<footer class="d-flex flex-wrap justify-content-center align-items-center py-3 my-4 border-top border-success">
+<footer class="d-flex flex-wrap justify-content-center align-items-center py-3 my-4 mt-5 border-top border-success">
     <div class="d-flex align-items-center pt-2">
         <span class="text-muted">Copyright of &copy;<?= SCHOOL_NAME . ', ' . date('Y') ?></span>
     </div>

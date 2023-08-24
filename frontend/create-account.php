671474db0d09302bcf9a658cf0bbbb4bc2de8d05
Isaac Machakata <isaac@caasi.co.zw>
2023-08-24 07:39:31 +0200

    feat: use footer component

--- frontend/create-account.php
+++ frontend/create-account.php
@@ -37,13 +37,7 @@ render_component('head', ['title' => 'Register']); ?>
             </div>
         </section>
     </main>
-    <footer class="d-flex flex-wrap justify-content-center align-items-center py-4 my-4 border-top border-success">
-        <div class="d-flex align-items-center">
-            <span class="text-muted">Copyright of &copy;<?= SCHOOL_NAME . ', ' . date('Y') ?></span>
-        </div>
-    </footer>
-
-    <script src="/assets/js/bootstrap.bundle.min.js"></script>
+    <?php render_component('footer'); ?>
 </body>
 
 </html>

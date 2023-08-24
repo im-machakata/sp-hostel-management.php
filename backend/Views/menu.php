5b9c06826abf2f476fcfd7b650496da67fd15c0b
Isaac Machakata <isaac@caasi.co.zw>
2023-08-24 12:26:15 +0200

    feat: add admin menus

--- backend/Views/menu.php
+++ backend/Views/menu.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-light border-bottom border-success shadow-sm">
     <div class="container-fluid">
         <a class="navbar-brand fw-bold" href="/"><span class="text-success">Hostel</span><span class="text-muted">ia</span></a>
-        <button class="navbar-toggler rounded" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler bg-warning" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
@@ -9,12 +9,18 @@
                 <li class="nav-item">
                     <a class="nav-link<?= url_active('/') ?>" aria-current="page" href="/">Home</a>
                 </li>
+                <?php if (session('UserType') == 'Admin') : ?>
+                    <li class="nav-item">
+                        <a class="nav-link<?= url_active('/booked-rooms.php') ?>" href="/booked-rooms.php">View Bookings</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link<?= url_active('/booked-rooms.php') ?>" href="/booked-rooms.php">Manage Rooms</a>
+                    </li>
+                <?php endif; ?>
                 <?php if (session('UserID')) : ?>
                     <li class="nav-item">
                         <a class="nav-link<?= url_active('/my-account.php') ?>" href="/my-account.php">My Account</a>
                     </li>
-                <?php endif; ?>
-                <?php if (session('UserID')) : ?>
                     <li class="nav-item">
                         <a class="nav-link<?= url_active('/logout.php') ?>" href="/logout.php">Logout</a>
                     </li>

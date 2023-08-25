a90a3e029291d89019baa4ec4568775b3ca6d8e9
Isaac Machakata <isaac@caasi.co.zw>
2023-08-25 15:52:49 +0200

    refactor: show buttons on manage page

--- backend/Views/room.php
+++ backend/Views/room.php
@@ -3,8 +3,18 @@
         <img src="<?= $image_url ?? ('/assets/images/demo.jpg') ?>" class="card-img-top img-fluid" alt="<?= $name ?> Image">
     </div>
     <div class="card-body">
-        <div class="text-dark h5 badge bg-warning mb-2">
-            USD $<?= number_format($cost, 2) ?>
+        <div class="clearfix mb-1">
+            <div class="text-dark h5 badge bg-warning mb-2">
+                USD $<?= number_format($cost, 2) ?>
+            </div>
+            <?php if (session('UserType') == 'Admin' && Request::isUrl('/manage-rooms.php')) : ?>
+                <div class="text-dark h5 badge bg-info mb-2">
+                    EDIT ROOM
+                </div>
+                <div class="h5 badge bg-danger mb-2 float-end">
+                    DELETE
+                </div>
+            <?php endif; ?>
         </div>
         <h2 class="card-title"><?= $name ?></h2>
         <p class="card-text"><?= $description ?></p>

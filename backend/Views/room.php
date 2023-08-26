34d340edd7a9829c67f8a0389c4731d1af1a54db
Isaac Machakata <isaac@caasi.co.zw>
2023-08-26 17:39:54 +0200

    refactor: add more form fields for room deletion

--- backend/Views/room.php
+++ backend/Views/room.php
@@ -9,12 +9,23 @@
             </div>
             <?php if (session('UserType') == 'Admin' && Request::isUrl('/manage-rooms.php')) : ?>
                 <div class="float-end">
-                    <a class="text-dark h5 badge bg-info mb-2 text-decoration-none" href="#editRoom" data-bs-toggle="modal" data-bs-target="#newRoom" data-id="<?= $id ?>" data-name="<?= $name ?>" data-details="<?= $description ?>" data-cost="<?= $cost ?>" data-image="<?= $image_url ?>" data-action="edit">
-                        EDIT
-                    </a>
-                    <span class="h5 badge bg-danger alert-link mb-2 text-decoration-none" data-id="<?= $id ?>">
-                        DELETE
-                    </span>
+                    <form action="/manage-rooms.php" method="post" class="d-block">
+                        <a 
+                            class="text-dark h5 badge bg-info mb-2 text-decoration-none" 
+                            href="#editRoom" data-bs-toggle="modal" 
+                            data-bs-target="#newRoom" 
+                            data-id="<?= $id ?>" 
+                            data-name="<?= $name ?>" 
+                            data-details="<?= $description ?>" 
+                            data-cost="<?= $cost ?>" 
+                            data-image="<?= $image_url ?>" 
+                            data-action="edit">
+                            EDIT
+                        </a>
+                        <input type="hidden" name="action" value="delete">
+                        <input type="hidden" name="id" value="<?= $id ?>">
+                        <button type="submit" class="btn badge bg-danger delete-room">DELETE</button>
+                    </form>
                 </div>
             <?php endif; ?>
         </div>

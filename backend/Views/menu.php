c2f3b9ba5f91d001fce97ec1ed00b9991e54db00
Isaac Machakata <isaac@caasi.co.zw>
2023-08-26 21:21:23 +0200

    feat: make menu sticky

--- backend/Views/menu.php
+++ backend/Views/menu.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-light border-bottom border-success shadow-sm">
+<nav class="navbar navbar-expand-lg navbar-light bg-white border-bottom border-success sticky-top shadow-sm">
     <div class="container-fluid">
         <a class="navbar-brand fw-bold" href="/"><span class="text-success">Hostel</span><span class="text-muted">ia</span></a>
         <button class="navbar-toggler bg-warning" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
